61d22d9450327d291cc49c8ba10e70b3767acefa
Wilf Engel <wilfengel@gmail.com>
2019-12-06 23:54:46 +0000

    fix navbar preview

--- src/cms/preview-templates/NavbarPreview.js
+++ src/cms/preview-templates/NavbarPreview.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import NavbarTemplate from '../../components/Navbar'
+import {NavbarTemplate} from '../../components/Navbar'
 
-const NavbarPreview = ({ entry, widgetFor }) => (
-    <NavbarTemplate
-    />
-)
+const NavbarPreview = ({ entry, widgetFor }) => {
+    const data = entry.getIn(["data"]).toJS();
+    return <NavbarTemplate data={data}/>
+}
 
 NavbarPreview.propTypes = {
     entry: PropTypes.shape({
